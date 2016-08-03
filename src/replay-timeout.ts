b0b3fc4dced761345f43cc871f39b5395ce400aa
Joe Watson <joe_watson@mail.com>
2016-08-03 07:50:49 +0100

    Tidied replay-timeout

--- src/replay-timeout.ts
+++ src/replay-timeout.ts
@@ -9,49 +9,49 @@ import { replayFailure } from "./handlers";
 import { State } from "./state";
 import { eventLog, timeout } from "./utils";
 
-/** State stored in the closure for each each replay timeout, when the timeout triggers, it calls the callback if state has not advanced */
+/** Portion of State that is relevant for checking if the timeout during replay phase should be acted on when it triggers */
 interface ReplayState {
   id: number;
   match: number | null;
-}
-function makeReplayState(state: State): ReplayState {
-  return {
-    id: state.id,
-    match: state.notesMatched
-  }
-}
-function showReplayState(r: ReplayState): string {
-  return `(${r.id}, ${r.match})`;
-}
-function equalReplayState(r1: ReplayState, r2: ReplayState) {
-  return r1.id === r2.id && r1.match === r2.match;
 }
 
 
 /** Establish timeout for replays */
 export function setReplayTimeout(state: State): void {
 
-  const r: ReplayState = makeReplayState(state);
+  const oldR: ReplayState = makeReplayState(state); // Relevant game state when the timeout is set
 
-  eventLog("TOSET", null, "Setting replay timeout at " + showReplayState(r));
+  eventLog("TOSET", null, "Setting replay timeout at " + showReplayState(oldR));
 
-  timeout(constants.durations.replayWait, makeReplayTimeoutCallback(state, r));
+  timeout(constants.durations.replayWait, () => {
 
-}
-
-/** Helper to create closure that is called when timeout triggers and during replay (user too slow to enter a note) */
-function makeReplayTimeoutCallback(state: State, oldR: ReplayState, ): () => void {
-
-  return () => {
-
-    const newR: ReplayState = makeReplayState(state);
+    const newR: ReplayState = makeReplayState(state); // Relevant game state when the timeout is triggered
 
     eventLog("TOCHK", null, `Was ${showReplayState(oldR)}, now ${showReplayState(newR)}`);
 
-    // If no progress has been made, and power is on
+    // If relevant game state has not changed and the power is on, then replay has failed
     if (state.power && equalReplayState(oldR, newR)) {
       eventLog("TOFAIL", null, "Failure triggered");
       replayFailure(state, null);
     }
-  };
+  });
+
+}
+
+/** Helper function to construct a ReplayState */
+function makeReplayState(state: State): ReplayState {
+  return {
+    id: state.id,
+    match: state.notesMatched
+  }
+}
+
+/** Helper function to produce a string representation of a ReplayState for debugging messages */
+function showReplayState(r: ReplayState): string {
+  return `(${r.id}, ${r.match})`;
+}
+
+/** Helper function to test two replay states for value equality */
+function equalReplayState(r1: ReplayState, r2: ReplayState) {
+  return r1.id === r2.id && r1.match === r2.match;
 }
